9c76bf16a34b1428e7d7b3e60efbec63b3324072
Epsiloong <144572295+epsiloong@users.noreply.github.com>
2024-10-09 19:52:54 -0400

    testing extensible blake3 for branch gen

--- src/v1.rs
+++ src/v1.rs
@@ -73,22 +73,10 @@ fn populate_cachehog(input: &[u8]) -> Result<[u8; MEMORY_SIZE], Error> {
 // Generate branch table
 fn populate_branch_table(input: &[u8]) -> [u64; 4096] {
     let mut output: [u8; 32768] = [0; 32768];
-    let mut hasher = Sha512::new();
-
+    let mut hasher = blake3::Hasher::new();
     hasher.update(input);
-
-    let mut last_hash: [u8; 64];
-    let mut hash_idx = 0;
-    while hash_idx < 512 {
-        last_hash = hasher.finalize_reset().into();
-        output[hash_idx * 64..(hash_idx + 1) * 64].copy_from_slice(last_hash.as_ref());
-        // Add some variation so it's not just a chained hash
-        for item in last_hash.iter_mut() {
-            *item = item.wrapping_mul(5);
-        }
-        hasher.update(last_hash);
-        hash_idx += 1;
-    }
+    let mut output_reader = hasher.finalize_xof();
+    output_reader.fill(&mut output);
 
     let output_u64: [u64; 4096] = unsafe { std::mem::transmute(output) };
 
